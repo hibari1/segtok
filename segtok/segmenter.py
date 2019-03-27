30369a2fad97f337c3a613cac5782f941e41a711
marcin <marcin.skowron@gmail.com>
2019-03-27 19:20:18 +0100

    extended list of abbreviations (focus: German language support)

--- segtok/segmenter.py
+++ segtok/segmenter.py
@@ -47,7 +47,13 @@ HYPHENS = '\u00AD\u058A\u05BE\u0F0C\u1400\u1806\u2010-\u2012\u2e17\u30A0-'
 # (such as "etc.")
 ABBREVIATIONS = """
 approx Capt cf Col Dr f\.?e figs? Gen e\.?g i\.?e i\.?v
-Mag med Mr Mrs Mt nat No nr p\.e phil prof rer
+MMag Mag med Mr Mrs Mt nat No nr p\.e phil prof rer Dipl Ing
+Abs Art Zl Fr Bf Z lt
+I II III IV V VI VII VIII IX X XI XII XIII XIV XV XVI XVII XVII XVIII XIX XX XXI XXII XXIII XXIV XXV XXVI XXVII XXVIII XXIX XXX XXXI XXXII XXXIII XXXIV XXXV XXXVI XXXVII XXXVIII XXXIX XXXX
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
+bzw vgl lit o.a u.a ua bspw gem überw cit
+Tz hg Inkl inkl Fa BGBl RGBl Anm Erk UmwG
+\)
 sci Sgt Sr Sra Srta St univ vol vs z\.B
 Jän Jan Ene Feb Mär Mar Apr Abr May Jun Jul Aug Sep Sept Oct Okt Nov Dic Dez Dec
 E\.U U\.K U\.S
