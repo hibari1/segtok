5b43ba030a7f4ed3f910e0efe5e44a9057e83d87
marcin <marcin.skowron@gmail.com>
2019-09-19 14:38:56 +0200

    extended abbr list

--- segtok/segmenter.py
+++ segtok/segmenter.py
@@ -52,7 +52,7 @@ Abs Art Zl Fr Bf Z lt
 I II III IV V VI VII VIII IX X XI XII XIII XIV XV XVI XVII XVII XVIII XIX XX XXI XXII XXIII XXIV XXV XXVI XXVII XXVIII XXIX XXX XXXI XXXII XXXIII XXXIV XXXV XXXVI XXXVII XXXVIII XXXIX XXXX
 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
 bzw vgl lit o.a u.a ua bspw gem überw cit
-Tz hg Inkl inkl Fa BGBl RGBl Anm Erk UmwG
+Tz hg Inkl inkl Fa BGBl RGBl Anm Erk UmwG LGBl
 \)
 sci Sgt Sr Sra Srta St univ vol vs z\.B
 Jän Jan Ene Feb Mär Mar Apr Abr May Jun Jul Aug Sep Sept Oct Okt Nov Dic Dez Dec
